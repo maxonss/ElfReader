f6d2e2a9f3a346aacfb08ba64862d8395f975752
Max <cruet.hostler_0h@icloud.com>
2024-05-01 13:31:23 +0200

    Add arguments (options) management
    - Option recognition on program call
    - Option structure definition and associated function
    - Usage helper function

---
+++ include/options.h
@@ -0,0 +1,21 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+// Définition de la structure pour stocker les informations sur chaque option
+typedef struct {
+    const char *name;          // Utilisation de const char * pour les chaînes constantes
+    void (*function)(char *);  // Type de fonction ajusté
+    int required_arg;          // Indique si l'option requiert un argument (1 = oui, 0 = non)
+} Option;
+
+// Déclaration des fonctions liées à chaque option
+void option_d(char *filename);
+void option_s(char *filename);
+void option_a(char *filename);
+void option_e(char *filename);
+void option_t(char *filename);
+void option_l(char *filename);
+void option_c(char *filename);
+void option_h();
+void print_basic_info(char *filename);
+#endif // OPTIONS_H
---
+++ src/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "../include/options.h"
+
+// Nombre d'options disponibles
+// TODO: Ajouter un count de options[]
+const int NB_OPTIONS = 7;
+
+// Définition du tableau d'options
+Option options[] = {
+        {"-d", option_d, 1},
+        {"-s", option_s, 1},
+        {"-a", option_a, 1},
+        {"-e", option_e, 1},
+        {"-t", option_t, 1},
+        {"-l", option_l, 1},
+        {"-h", option_h, 0}
+};
+
+int main(int argc, char *argv[]) {
+    // Vérifier que le nom du fichier ELF a été fourni
+    if (argc < 2) {
+        printf("Erreur: veuillez fournir au moins un nom de fichier.\n");
+        option_h();
+        return 1;
+    }
+
+    // Stocker le nom du fichier ELF
+    char *filename = argv[1];
+
+    // Si aucun argument autre que le nom du fichier n'est passé, afficher les informations standards du fichier ELF
+    if (argc == 2) {
+        print_basic_info(filename);
+        return 0;
+    }
+
+    // Parcourir les arguments passés en ligne de commande
+    for (int i = 2; i < argc; i++) {
+        // Vérifier si l'argument correspond à une option
+        bool option_found = false;
+
+        for (int j = 0; j < NB_OPTIONS; j++) {
+            if (strcmp(argv[i], options[j].name) == 0) {
+                // Appeler la fonction liée à l'option
+                options[j].function(filename);
+                option_found = true;
+                break;
+            }
+        }
+
+        if (!option_found) {
+            // Si l'argument ne correspond à aucune option, afficher un message d'erreur et quitter le programme
+            printf("Erreur : option invalide '%s'\n", argv[i]);
+            option_h();
+            return 1;
+        }
+    }
+
+    return 0;
+}
